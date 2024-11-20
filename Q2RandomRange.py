a99724b5e9ada94210a797f1243721476e1bac6c
Drew Ferguson <drew.ferguson@stu.ocsb.ca>
2024-11-20 15:25:01 +0000

    Unit 5-01

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,5 +1,6 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+
 import random
 
 start = float(input("Enter the starting number of the range: "))
