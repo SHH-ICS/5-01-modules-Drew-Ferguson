ed038bb1ff4e70f27036f5164ae654ffefae534b
Drew Ferguson <drew.ferguson@stu.ocsb.ca>
2024-11-20 15:21:38 +0000

    Unit 5-01

--- Q4Picture.py
+++ Q4Picture.py
@@ -5,17 +5,29 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
-pygame.display.set_caption('Simple Picture')
+pygame.display.set_caption('Pizza Design')
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+LIGHT_BLUE = (173, 216, 230)
+PIZZA_YELLOW = (255, 223, 132)
+CRUST_BROWN = (205, 133, 63)
+RED = (255, 0, 0) 
+BOX_GRAY = (192, 192, 192)
 
-screen.fill(WHITE)
+screen.fill(LIGHT_BLUE)
 
-pygame.draw.rect(screen, RED, (50, 50, 300, 200))
+pygame.draw.rect(screen, BOX_GRAY, (50, 50, 300, 300)) 
 
-pygame.draw.circle(screen, BLUE, (200, 300), 50)
+pygame.draw.circle(screen, CRUST_BROWN, (200, 200), 130) 
+pygame.draw.circle(screen, PIZZA_YELLOW, (200, 200), 120)  
+
+toppings = [(160, 160), (240, 160), (200, 240), (150, 220), (250, 220)]
+for topping in toppings:
+    pygame.draw.circle(screen, RED, topping, 10)
+
+pygame.draw.polygon(screen, PIZZA_YELLOW, [(200, 200), (130, 350), (270, 350)]) 
+pygame.draw.lines(screen, CRUST_BROWN, True, [(200, 200), (130, 350), (270, 350)], 10) 
+
+pygame.draw.circle(screen, RED, (200, 280), 15)
 
 pygame.display.flip()
 
